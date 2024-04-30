0ccc0ef502a2f8d84f30af510fbce6def179a0a9
Кутибашвили Илья <generals22@mail.ru>
2024-04-30 16:04:37 +0300

    func1

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def capital_letters(input_string):
   return input_string.upper()
+
+def firstletters(sentence):
+    '''
+       Функция которая делает заглавные буквы в строке
+       '''
+
+    return ' '.join(word.capitalize() for word in sentence.split())
